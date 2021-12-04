7bb971f503d9693f577d5a1a54ec673f4ed2fc12
charliehigginsiii <charliehiggiii@gmail.com>
2021-12-04 13:32:04 -0500

    3D arrays are now working

--- src/application/FileOperators.java
+++ src/application/FileOperators.java
@@ -139,19 +139,41 @@ public class FileOperators {
         			||listener.tokens.get(i-6).contentEquals("long")
         			||listener.tokens.get(i-6).contentEquals("char"))) {
         		Collections.swap(listener.tokens,i-2,i-1);
+        	}if(listener.tokens.get(i).contentEquals("=")
+        			&&listener.tokens.get(i-1).contentEquals("]")
+        			&&listener.tokens.get(i-3).contentEquals("[")
+        			&&(listener.tokens.get(i-8).contentEquals("int")
+        			||listener.tokens.get(i-8).contentEquals("String")
+        			||listener.tokens.get(i-8).contentEquals("double")
+        			||listener.tokens.get(i-8).contentEquals("float")
+        			||listener.tokens.get(i-8).contentEquals("boolean")
+        			||listener.tokens.get(i-8).contentEquals("short")
+        			||listener.tokens.get(i-8).contentEquals("long")
+        			||listener.tokens.get(i-8).contentEquals("char"))) {
+        		Collections.swap(listener.tokens,i-1,i-2);
+        	}if(listener.tokens.get(i).contentEquals("length")
+        			&&listener.tokens.get(i-1).contentEquals(".")
+        			&&listener.tokens.get(i-2).contentEquals("]")
+        			&&listener.tokens.get(i-20).contentEquals("GetLength(1)")) {
+        		listener.tokens.set(i, "GetLength(2)");
+        		listener.tokens.remove(i-2);
+        		listener.tokens.remove(i-3);
+        		listener.tokens.remove(i-4);
+        	}if(listener.tokens.get(i).contentEquals("length")
+        			&&listener.tokens.get(i-1).contentEquals(".")
+        			&&listener.tokens.get(i-2).contentEquals("]")
+        			&&listener.tokens.get(i-20).contentEquals("GetLength(0)")) {
+        		listener.tokens.set(i, "GetLength(1)");
+        		listener.tokens.remove(i-2);
+        		listener.tokens.remove(i-3);
+        		listener.tokens.remove(i-4);
         	}if(listener.tokens.get(i).contentEquals("length")
         			&&listener.tokens.get(i+4).contentEquals(")")
         			&&listener.tokens.get(i+20).contentEquals("length")) {
         		listener.tokens.set(i,"GetLength(0)");
         	}
-        	if(listener.tokens.get(i).contentEquals("length")
-        			&&listener.tokens.get(i-1).contentEquals(".")
-        			&&listener.tokens.get(i-2).contentEquals("]")) {
-        		listener.tokens.set(i, "GetLength(1)");
-        		listener.tokens.remove(i-2);
-        		listener.tokens.remove(i-3);
-        		listener.tokens.remove(i-4);
-        	}
+        	
+        		
         }
         translation +=("using System;\n");
         
